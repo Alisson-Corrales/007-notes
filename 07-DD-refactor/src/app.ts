7736c52bf1c55f33b4906edb03bbe5abbcc80114
Alisson Corrales <Acorra435@west-mec.local>
2022-01-20 18:28:45 -0700

    pushing

---
+++ 07-DD-refactor/src/app.ts
@@ -0,0 +1,6 @@
+import ProjectInput from "./components/project-input.js"
+import ProjectList from "./components/project-list"
+
+const ProjectInput();
+const activeProjects = new ProjectList("active");
+const finishedProjects = new ProjectList("finished");
